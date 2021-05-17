c84d7038df86d459d182dc39609a0f17e9059e9d
tahaiw <81987118+tahaiw@users.noreply.github.com>
2021-05-17 13:31:40 +0300

    ade

---
+++ baslat.bat
@@ -0,0 +1,8 @@
+echo off
+title dev by where was i
+color a
+cls
+:a
+node main.js
+goto a
+pause
--- keep_alive.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 6969;
-app.get('/', (req, res) => res.send('Harmony Alive!'));
-
-app.listen(port, () => console.log(`Harmony streaming to http://localhost:${port}`));
